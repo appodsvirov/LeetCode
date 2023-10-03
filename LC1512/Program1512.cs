f6a96fad001caf56e6858de9f94a29f269aebe50
USER <appodsvirov@gmail.com>
2023-10-03 15:38:52 +0300

    Accepted 1512

--- LC1512/Program1512.cs
+++ LC1512/Program1512.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+
+Solution s = new Solution();
+
+int[] nums = new int[] {
+    1,2,3
+};
+Console.WriteLine(s.NumIdenticalPairs(nums));
+
+
+public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        Dictionary<int, List<int>> numAndIndices = new Dictionary<int, List<int>>();
+        int result = 0;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (numAndIndices.ContainsKey(nums[i]))
+            {
+                foreach (int j in numAndIndices[nums[i]])
+                {
+                    if(i < j)
+                    {
+                        result++;
+                    }    
+                }
+                numAndIndices[nums[i]].Add(i);
+            }
+            else
+            {
+                numAndIndices[nums[i]] = new List<int>() { i };
+            }
+        }
+        return result;
+    }
+}
