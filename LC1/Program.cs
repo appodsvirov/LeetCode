70bb6e89dc642a85641d0157d5a1d1cdfaa93173
USER <appodsvirov@gmail.com>
2023-10-03 15:46:14 +0300

    add LC1

---
+++ LC1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+Solution s = new Solution();
+Console.WriteLine(string.Join(" ",s.TwoSum(new int[] {2, 7, 11, 15 }, 9)));
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int value;
+
+            if (dict.TryGetValue(nums[i], out value))
+            {
+                result[0] = i;
+                result[1] = value;
+                return result;
+            }
+            dict.TryAdd(target - nums[i], i);
+        }
+        throw new System.Exception();
+    }
+}
