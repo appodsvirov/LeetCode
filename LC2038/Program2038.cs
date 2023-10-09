1be5430757b9cecd10d181cd509877bb87209573
USER <appodsvirov@gmail.com>
2023-10-09 18:31:41 +0300

    Accepted LC2038

---
+++ LC2038/Program2038.cs
@@ -0,0 +1,25 @@
+﻿Solution s = new Solution();
+Console.WriteLine(s.WinnerOfGame("AAAABBBB"));
+public class Solution
+{
+    public bool WinnerOfGame(string colors)
+    {
+        int countBobMove = 0, countAliceMove = 0;
+        for (int i = 1; i < colors.Length - 1; i++)
+        {
+            if (colors[i - 1] == 'A' &&
+            colors[i] == 'A' &&
+            colors[i + 1] == 'A')
+            {
+                countAliceMove++;
+            }
+            else if (colors[i - 1] == 'B' &&
+            colors[i] == 'B' &&
+            colors[i + 1] == 'B')
+            {
+                countBobMove++;
+            }
+        }
+        return (countAliceMove > countBobMove);
+    }
+}
