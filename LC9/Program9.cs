0736f67124d8cacc43b1788903d6748c53f2cd6a
USER <appodsvirov@gmail.com>
2023-10-09 14:14:51 +0300

    Accepted LC9

---
+++ LC9/Program9.cs
@@ -0,0 +1,26 @@
+﻿
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        if (x == 0) return true;
+
+        int[] arrX = new int[50];
+        int first = 0;
+        int second = 0;
+        for (int i = 0; x != 0; x /= 10, i++)
+        {
+            second = i;
+            arrX[i] = x % 10;
+        }
+        while (true)
+        {
+            if (first >= second) return true;
+            if (arrX[first] != arrX[second]) return false;
+            first++;
+            second--;
+        }
+        return false;
+    }
+}
