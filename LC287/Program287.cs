36bbe07a41754087d6e2506d88da6c5fa5ff6c6f
USER <appodsvirov@gmail.com>
2023-10-12 20:04:35 +0300

    LC287

---
+++ LC287/Program287.cs
@@ -0,0 +1,20 @@
+﻿
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        int[] greedyDict = new int[200000];
+        foreach (int num in nums)
+        {
+            if (greedyDict[num] != 0)
+            {
+                return num;
+            }
+            else
+            {
+                greedyDict[num]++;
+            }
+        }
+        throw new Exception();
+    }
+}
