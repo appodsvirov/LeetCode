643516c7d9bc9c94159781cbfe18c7576862e9a3
USER <appodsvirov@gmail.com>
2023-10-16 18:18:34 +0300

    Accepted LC70

---
+++ LC70/Program70.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1) return 1;
+        int past = 2, pastPast = 1, current = 2;
+
+        for (int i = 3; i <= n; i++, pastPast = past, past = current)
+        {
+            current = past + pastPast;
+        }
+        return current;
+    }
+}
