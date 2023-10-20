a4e8356421e2425b19b0cc585867818f072317e8
USER <appodsvirov@gmail.com>
2023-10-20 15:54:22 +0300

    Accepted LC14

---
+++ LC14/Program14.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        StringBuilder result = new StringBuilder();
+        int minLength = int.MaxValue;
+        foreach (string str in strs)
+        {
+            minLength = Math.Min(minLength, str.Length);
+        }
+        bool check;
+        for (int i = 0; i < minLength; i++)
+        {
+            check = true;
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (strs[j - 1][i] != strs[j][i])
+                {
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+            {
+                result.Append(strs[0][i]);
+            }
+            else
+            {
+                break;
+            }
+        }
+        return result.ToString();
+    }
+}
