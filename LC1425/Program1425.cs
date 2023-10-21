e839eef04858cfcde57b1c4e3a5541dac21f80eb
USER <appodsvirov@gmail.com>
2023-10-21 18:11:32 +0300

    Accepted LC 1425

---
+++ LC1425/Program1425.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int ConstrainedSubsetSum(int[] nums, int k)
+    {
+        LinkedList<int> dq = new LinkedList<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dq.Count > 0)
+            {
+                nums[i] += nums[dq.First.Value];
+            }
+            while (dq.Count > 0 &&
+                (i - dq.First.Value >= k || nums[i] >= nums[dq.Last.Value]))
+            {
+                if (nums[i] >= nums[dq.Last.Value])
+                {
+                    dq.RemoveLast();
+                }
+                else
+                {
+                    dq.RemoveFirst();
+                }
+            }
+
+            if (nums[i] > 0)
+            {
+                dq.AddLast(i);
+            }
+        }
+        return nums.Max(); // (O(n))
+    }
+}
