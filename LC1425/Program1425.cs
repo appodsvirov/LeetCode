73c93bf115cc0486ec932c194a0ea985b6d66c31
USER <appodsvirov@gmail.com>
2023-10-22 15:31:42 +0300

    LC 1793

--- LC1425/Program1425.cs
+++ LC1425/Program1425.cs
@@ -6,15 +6,20 @@ public class Solution
 {
     public int ConstrainedSubsetSum(int[] nums, int k)
     {
+        /*
+         * дек будет хранить окно индексов
+         **/
         LinkedList<int> dq = new LinkedList<int>();
+        
         for (int i = 0; i < nums.Length; i++)
         {
             if (dq.Count > 0)
             {
                 nums[i] += nums[dq.First.Value];
             }
+            // уменьшаем окно, пока условие с k не выполнится
             while (dq.Count > 0 &&
-                (i - dq.First.Value >= k || nums[i] >= nums[dq.Last.Value]))
+                (i - dq.First.Value >= k || nums[i] >= nums[dq.Last.Value])) 
             {
                 if (nums[i] >= nums[dq.Last.Value])
                 {
@@ -25,8 +30,8 @@ public class Solution
                     dq.RemoveFirst();
                 }
             }
-
-            if (nums[i] > 0)
+            // отицательные нет смысла добавлять, т.к. если в nums нет других, то в конце nums.Max() выберет 1 число 
+            if (nums[i] > 0) 
             {
                 dq.AddLast(i);
             }
---
+++ LC1793/Program1793.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int MaximumScore(int[] nums, int k)
+    {
+        int left = k, right = k; // старт из k
+        int minValue = nums[k], result = nums[k];
+        while (left > 0 || right < nums.Length - 1)
+        {
+            // сначала k, а потом
+            // k, (k+1) или  (k-1), k в зависимости от Max(k-1, k+1)
+            // и так далее.. 
+            if (left == 0 || (right < nums.Length - 1 && nums[right + 1] > nums[left - 1]))
+            {
+                right++;
+                minValue = Math.Min(minValue, nums[right]);
+            }
+            else
+            {
+                left--;
+                minValue = Math.Min(minValue, nums[left]);
+            }
+            // для subArray = {nums[k]}: result = k*(k-k+1) = k
+            result = Math.Max(result, minValue * (right - left + 1));
+        }
+
+        return result;
+    }
+}
