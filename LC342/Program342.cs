759ebf074d415aa3a73949f767d49c4aa7d5f3d6
USER <appodsvirov@gmail.com>
2023-10-24 12:00:35 +0300

    Accepted LC342

---
+++ LC342/Program342.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    //0x55555555 =
+    // 0101 0101 0101 0101 0101 0101 0101 0101
+    public bool IsPowerOfFour(int n) =>
+    (n & 0x55555555) != 0 && 
+    (n & (n - 1)) == 0
+    ;
+}
