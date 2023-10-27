05a88860bfe1f4e687db835f3c1d78ad9884b624
USER <appodsvirov@gmail.com>
2023-10-27 11:30:32 +0300

    LC5(h1)

--- LC5/Program5.cs
+++ LC5/Program5.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+public class Solution
+{
+    public string LongestPalindrome(string s)
+    {
+        if(s is null && s?.Length == 0)
+        {
+            return "";
+        }
+        // ^#[0]#[1]#[2]#[3]#[4]#[5]#...[n-3]#[n-2]#[n-1]#$
+        StringBuilder metaS = new StringBuilder("^#");
+        foreach (char c in s)
+        {
+            metaS.Append(c).Append("#");
+        }
+        metaS.Append("$");
+        string modifiedString = metaS.ToString();
+
+        int strLength = modifiedString.Length;
+        int[] palindromeLengths = new int[strLength];
+        int center = 0;  
+        int rightEdge = 0;  
+
+        for (int i = 1; i < strLength - 1; i++)
+        {
+            palindromeLengths[i] = (rightEdge > i) ? Math.Min(rightEdge - i, palindromeLengths[2 * center - i]) : 0;
+
+            while (modifiedString[i + 1 + palindromeLengths[i]] == modifiedString[i - 1 - palindromeLengths[i]])
+            {
+                palindromeLengths[i]++;
+            }
+
+            if (i + palindromeLengths[i] > rightEdge)
+            {
+                center = i;
+                rightEdge = i + palindromeLengths[i];
+            }
+        }
+        int maxLength = 0;
+        int maxCenter = 0;
+        for (int i = 0; i < strLength; i++)
+        {
+            if (palindromeLengths[i] > maxLength)
+            {
+                maxLength = palindromeLengths[i];
+                maxCenter = i;
+            }
+        }
+        int start = (maxCenter - maxLength) / 2;
+        int end = start + maxLength;
+
+        return s.Substring(start, end - start);
+    }
+}
