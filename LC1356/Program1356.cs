90d436fa8d7bf0db4e90e5031f035507f20eedd1
USER <appodsvirov@gmail.com>
2023-10-30 13:54:57 +0300

    LC1356 + LC458

---
+++ LC1356/Program1356.cs
@@ -0,0 +1,34 @@
+﻿
+
+Solution solution = new Solution();
+
+int[] ints = new int[] { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+Console.WriteLine(string.Join(" ", ints));
+Console.WriteLine(string.Join(" ", solution.SortByBits(ints)));
+
+
+public class Solution
+{
+    public int[] SortByBits(int[] arr)
+    {
+        int[] result = new int[arr.Length];
+
+        for(int i = 1;  i <= 10000; i <<= 1)
+        {
+            for(int j = 0; j < arr.Length; j++)
+            {
+                if( (i & arr[j]) != 0)
+                {
+                    result[j]++;
+                }
+            }
+        }
+        var pairs = result.Zip(arr, (r, a) => new { Result = r, Value = a });
+        var sortedPairs = pairs.OrderBy(p => p.Result).ThenBy(p => p.Value);
+        result = sortedPairs.Select(p => p.Result).ToArray();
+        arr = sortedPairs.Select(p => p.Value).ToArray();
+
+        return arr;
+
+    }
+}
---
+++ LC458/Program458.cs
@@ -0,0 +1,13 @@
+﻿Solution s = new Solution();
+
+Console.WriteLine(s.PoorPigs(4, 29, 15));
+public class Solution
+{
+    public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+    {
+        int result = 0;
+        int div = (minutesToTest / minutesToDie) + 1;
+        for (; Math.Pow(div, result) < buckets; result++) ;
+        return result;
+    }
+}
