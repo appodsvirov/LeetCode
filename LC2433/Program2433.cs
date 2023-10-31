cae2332fe8d2c875a35090bee188efa7c985bf22
USER <appodsvirov@gmail.com>
2023-10-31 12:28:57 +0300

    LC2433

---
+++ LC2433/Program2433.cs
@@ -0,0 +1,26 @@
+﻿
+
+Solution s = new Solution();
+Console.WriteLine(string.Join(" ", s.FindArray(new int[] { 13}) ));
+
+public class Solution
+{
+
+    public int[] FindArray(int[] pref)
+    {
+        if (pref is null && pref?.Length == 0)
+        {
+            return new int[0];
+        }
+        int[] result = new int[pref.Length];
+        
+        int last = result[0] = pref[0];
+        for (int i = 1; i < pref.Length; i++)
+        {
+            result[i] = last^pref[i];
+            last ^= result[i];
+        }
+
+        return result;
+    }
+}
