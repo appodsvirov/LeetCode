f5863920dd593dd665f6a2e73b091f3c65ea135b
USER <appodsvirov@gmail.com>
2023-11-03 14:03:20 +0300

    Accepted LC 1441

---
+++ LC1441/Program1441.cs
@@ -0,0 +1,23 @@
+﻿Solution s = new Solution();
+
+Console.WriteLine(string.Join(" ", s.BuildArray(new int[] {1,2, 3 }, 3)));
+
+public class Solution
+{
+    public IList<string> BuildArray(int[] target, int n)
+    {
+        List<string> result = new List<string>();
+        for (int i = 0, j = 1; i < target.Length; i++)
+        {
+            while (target[i] > j)
+            {
+                j = (j + 1) % (n+1);
+                result.Add("Push");
+                result.Add("Pop");
+            }
+            result.Add("Push");
+            j = (j + 1) % (n + 1);
+        }
+        return result;
+    }
+}
