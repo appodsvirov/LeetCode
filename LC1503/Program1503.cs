54374c73d9c7e35ed7c4d6ebb552a38132c76e94
USER <appodsvirov@gmail.com>
2023-11-04 15:54:21 +0300

    Accepted LC1503

---
+++ LC1503/Program1503.cs
@@ -0,0 +1,8 @@
+﻿
+public class Solution
+{
+    public int GetLastMoment(int n, int[] left, int[] right)
+    {
+        return Math.Max((left.Length > 0) ? left.Max() : int.MinValue, (right.Length > 0) ? n - right.Min() : int.MinValue);
+    }
+}
