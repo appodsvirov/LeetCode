6d00fedb05ddd04d50b7abcf56b17e4d19f96c6c
USER <appodsvirov@gmail.com>
2023-11-05 17:00:42 +0300

    Accepted LC1535

---
+++ LC1535/Program1535.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int GetWinner(int[] arr, int k)
+    {
+        int max = Math.Max(arr[0], arr[1]);
+        for (int i = 2, j = 1; i < arr.Length; i++)
+        {
+            if (j == k)
+            {
+                return max;
+            }
+            if (max < arr[i])
+            {
+                max = arr[i];
+                j = 1;
+            }
+            else
+            {
+                j++;
+            }
+
+        }
+
+        return max;
+    }
+}
