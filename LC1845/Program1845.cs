2d216b6fead76095367af3cc41866c2d1bfd1376
USER <appodsvirov@gmail.com>
2023-11-06 15:02:40 +0300

    LC1845

---
+++ LC1845/Program1845.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class SeatManager
+{
+    /// <summary> Стек непрерывной последовательности. Без удаления "не top элементов" </summary>
+    private int topStack; 
+    /// <summary>Соритрованная куча удаленных "не top элементов"</summary>
+    private SortedSet<int> heap;
+
+    public SeatManager(int n)
+    {   
+        topStack = 0;
+        heap = new SortedSet<int>();
+    }
+
+    public int Reserve()
+    {
+        if (heap.Count == 0) // heap пуст => min элемент = top, т.е. ранее не удалялись "не top элементы"
+        {
+            return ++topStack;
+        }
+        else // heap не пуст, значит искомое место находится в heap.Min
+        {
+            int min = heap.Min();
+            heap.Remove(heap.Min);
+            return min;
+        }
+    }
+
+    public void Unreserve(int seatNumber)
+    {
+        if (seatNumber == topStack) // seatNumber -- "топ элемент",  просто удаляем верхушку стека
+        {
+            --topStack;
+        }
+        else
+        {
+            heap.Add(seatNumber); // в heap попадает удаленный "не топ элемент"
+        }
+    }
+}
