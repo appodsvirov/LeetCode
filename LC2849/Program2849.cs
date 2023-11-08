3c1641d1340c04a1608a25cc59b4657664fd6016
USER <appodsvirov@gmail.com>
2023-11-08 11:38:48 +0300

    Accepted LC2849

---
+++ LC2849/Program2849.cs
@@ -0,0 +1,11 @@
+﻿
+public class Solution
+{
+    public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+    {
+        int dist = Math.Max(Math.Abs(fx - sx), Math.Abs(fy - sy));
+        if (dist == 0 && (t == 0 || t == 2)) return true;
+        if (dist == 0 && t == 1) return false;
+        return dist <= t;
+    }
+}
