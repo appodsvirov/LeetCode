88393af633bb9936214cb6b994d17e87fa715c1c
USER <appodsvirov@gmail.com>
2023-11-09 21:36:04 +0300

    LC1759

---
+++ LC1759/Program1759.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Solution
+{
+    public int CountHomogenous(string s)
+    {
+        if (s.Length == 1) return 1;
+        int add = 0, result = s.Length;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                add = (add + 1) % (1000000007);
+                result = (result + add) % (1000000007);
+            }
+            else
+            {
+                add = 0;
+            }
+        }
+        return result;
+    }
+}
