b37e7f2be6e8865b8d48e44deadae160f8e5ab09
USER <appodsvirov@gmail.com>
2023-11-10 15:46:05 +0300

    LC1743

---
+++ LC1743/Program1743.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int[] RestoreArray(int[][] adjacentPairs)
+    {
+        Dictionary<int, List<int>> iDsDict = new Dictionary<int, List<int>>();
+
+        foreach (int[] ch in adjacentPairs)
+        {
+            if (!iDsDict.ContainsKey(ch[0]))
+            {
+                iDsDict[ch[0]] = new List<int>();
+            }
+            if (!iDsDict.ContainsKey(ch[1]))
+            {
+                iDsDict[ch[1]] = new List<int>();
+            }
+            iDsDict[ch[0]].Add(ch[1]);
+            iDsDict[ch[1]].Add(ch[0]);
+        }
+
+        int start = 0;
+        foreach (var ch in iDsDict)
+        {
+            if (ch.Value.Count == 1)
+            {
+                start = ch.Key;
+                break;
+            }
+        }
+
+        int n = adjacentPairs.Length + 1;
+        int[] result = new int[n];
+        result[0] = start;
+        result[1] = iDsDict[start][0];
+
+        for (int i = 2; i < n; i++)
+        {
+            var list = iDsDict[result[i - 1]];
+            result[i] = result[i - 2] == list[0] ? list[1] : list[0];
+        }
+
+        return result;
+    }
+}
