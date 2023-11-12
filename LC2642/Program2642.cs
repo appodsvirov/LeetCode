bdb7e634838c5bd13304b52324e06b12b3f7af50
USER <appodsvirov@gmail.com>
2023-11-12 13:51:25 +0300

    BFS

--- LC2642/Program2642.cs
+++ LC2642/Program2642.cs
@@ -1,71 +1,71 @@
-﻿
-public class Graph
+﻿public class Solution
 {
-    private List<List<(int, int)>> graph = new List<List<(int, int)>>();
-
-    public Graph(int n, int[][] edges)
+    public int NumBusesToDestination(int[][] routes, int source, int target)
     {
-        for (int i = 0; i < n; i++)
+        if (source == target) return 0; // мы на месте
+
+        //graph содержит пыры= key: номер остановки, value: List автобусов, которые проходят через данную остановку
+        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+        for (int i = 0; i < routes.Length; i++)
         {
-            graph.Add(new List<(int, int)>());
-        }
-
-        foreach(var edge in edges)
-        {
-            AddEdge(edge);
-        }
-    }
-
-    public void AddEdge(int[] edge)
-    {
-        graph[edge[0]].Add((edge[1], edge[2]));
-    }
-
-    public int ShortestPath(int node1, int node2)
-    {
-        //Алгоритм Дейкстры
-        int n = graph.Count;
-
-        var pq = new PriorityQueue<List<int>, List<int>>(
-            Comparer<List<int>>.Create((a, b) => a[0].CompareTo(b[0])));
-
-        //Инициализация: метка oo для всех node1 элементов, для node 1 метка = 0 
-        int[] labels = Enumerable.Repeat(int.MaxValue, n).ToArray();
-        labels[node1] = 0;
-
-
-        int lab, vertex;
-        var root = new List<int>() { 0, node1 };
-        pq.Enqueue(root, root);
-
-        while (pq.Count > 0) //Если все вершины посещены, алгоритм завершается. 
-        {
-            var curr = pq.Dequeue();
-            lab = curr[0];
-            vertex = curr[1];
-            //Если полученное значение длины меньше значения метки соседа, заменим значение метки полученным значением длины.
-            if (lab > labels[vertex])
+            foreach (int v in routes[i])
             {
-                continue;
-            }
-            if (vertex == node2)
-            {
-                return lab; //Доп. остновка: Алгоритм Дейкстры находит кратчайшие пути от node1 до ВСЕХ вершин. А нам надо найти только до node2
-            }
-            foreach (var node in graph[vertex]) //выбирается вершина u, имеющая минимальную метку.
-            {
-                int neighborNode = node.Item1;
-                int cost = node.Item2;
-                int newCost = lab + cost;
-                //Если полученное значение длины меньше значения метки соседа, заменим значение метки полученным значением длины.
-                if (newCost < labels[neighborNode])
+                if (!graph.ContainsKey(v))
                 {
-                    labels[neighborNode] = newCost;
-                    var newPath = new List<int>() { newCost, neighborNode };
-                    pq.Enqueue(newPath, newPath);
+                    graph[v] = new List<int>();
+                }
+                graph[v].Add(i);
+            }
+        }
+
+        Queue<int> queue = new Queue<int>();
+        HashSet<int> logStops = new HashSet<int>(); // уже посещенные остановки
+        HashSet<int> logBuses = new HashSet<int>(); // уже посещенные автобусы
+
+        queue.Enqueue(source);
+        logStops.Add(source);
+
+        int result = 0;
+
+        while (queue.Count > 0)
+        {
+            int stopsAtThisLevel = queue.Count;
+
+            for (int i = 0; i < stopsAtThisLevel; i++)
+            {
+                int currentStop = queue.Dequeue();
+
+                List<int> busesAtStop = (graph.ContainsKey(currentStop)) ? graph[currentStop] : new List<int>();
+
+                foreach (int bus in busesAtStop)
+                {
+                    if (logBuses.Contains(bus)) // уже катались на нем
+                    {
+                        continue; 
+                    }
+
+                    logBuses.Add(bus); 
+
+                    foreach (int nextStop in routes[bus]) // обход в ширину
+                    {
+                        if (logStops.Contains(nextStop))// уже были на этой остановке
+                        {
+                            continue;
+                        }
+
+                        logStops.Add(nextStop);
+
+                        if (nextStop == target) return result + 1;
+
+                        queue.Enqueue(nextStop);
+                    }
                 }
             }
+
+            result++;
         }
+
         return -1;
     }
 }
---
+++ LC815/Program815.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    private List<List<(int, int)>> graph = new List<List<(int, int)>>();
+    public int NumBusesToDestination(int[][] routes, int source, int target)
+    {
+        for (int i = 0; i < routes.Length; i++)
+        {
+            for (int j = 0; j < routes[i].Length; j++)
+            {
+
+            }
+        }
+    }
+}
