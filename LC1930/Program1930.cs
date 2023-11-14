5396e5ecdfef2218ad940503c61105789e9dedf8
USER <appodsvirov@gmail.com>
2023-11-14 14:57:38 +0300

    LC1930

---
+++ LC1930/Program1930.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections;
+using static Solution;
+
+
+
+Solution s = new Solution();
+Console.WriteLine(s.CountPalindromicSubsequence("bbcbaba"));
+
+public class Package
+{
+    public HashSet<int> UniqueChars { get; private set; } = new HashSet<int>();
+    public int СountPalindromes { get; private set; }
+
+    public void UpdateСountPalindromes()
+    {
+        СountPalindromes = UniqueChars.Count;
+    }
+}
+public class Solution
+{
+
+    public int CountPalindromicSubsequence(string s)
+    {
+        Dictionary<char, Package> dict = new Dictionary<char, Package>();
+        int result = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            if (dict.ContainsKey(s[i]))
+            {
+                dict[s[i]].UpdateСountPalindromes();
+                foreach (var kv in dict)
+                {
+                    kv.Value.UniqueChars.Add(s[i]);
+                }
+            }
+            else
+            {
+                foreach (var kv in dict)
+                {
+                    kv.Value.UniqueChars.Add(s[i]);
+                }
+                dict[s[i]] = new Package();
+            }
+        }
+
+        foreach (Package package in dict.Values)
+        {
+            result += package.СountPalindromes;
+        }
+
+
+        return result;////---------
+    }
+}
