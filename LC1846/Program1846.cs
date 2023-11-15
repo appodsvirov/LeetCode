30ac89afdf7bd8faffc2f22a5a007040c1c23fb4
USER <appodsvirov@gmail.com>
2023-11-15 14:03:40 +0300

    LC 1846

---
+++ LC1846/Program1846.cs
@@ -0,0 +1,20 @@
+﻿
+Solution s = new Solution();
+Console.WriteLine(s.MaximumElementAfterDecrementingAndRearranging(new int[] { 2, 2, 1, 2, 1 }));
+public class Solution
+{
+    public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+    {
+        //return arr.OrderBy(x => x).Aggregate(1, (a, b) => b - a <= 1 ? b : a + 1);
+        Array.Sort(arr);
+        arr[0] = 1;
+        for(int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > arr[i-1])
+            {
+                arr[i] = arr[i-1] + 1;
+            }
+        }
+        return arr[arr.Length-1];
+    }
+}
