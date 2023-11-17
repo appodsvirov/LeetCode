533f66d83b68b9837b6dfc744b119300acc6d25e
USER <appodsvirov@gmail.com>
2023-11-17 10:33:06 +0300

    LC1877

---
+++ LC1877/Program1877.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int MinPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+        int result = int.MinValue;
+        for (int i = 0, j = nums.Length - 1; i < j; i++, j--)
+        {
+            int sub = nums[j] + nums[i];
+            if (sub > result)
+            {
+                result = sub;
+            }
+        }
+        return result;
+    }
+}
